e8e46d2703acc66ecac31473c6cb0f1b708ca5b3
Martin Jackson <martin.mellum.jackson@gmail.com>
2015-04-16 08:48:45 +0200

    Ground work finished for HomeInsurance.java.

---
+++ src/baminsurances/data/HomeInsurance.java
@@ -0,0 +1,220 @@
+package baminsurances.data;
+
+/**
+ * A class that represents a home insurance in the company's data bank.
+ * 
+ * @author martin
+ */
+public class HomeInsurance extends Insurance {
+    private String streetAddress;
+    private String zipCode;
+    private int constructionYear;
+    private String homeType;
+    private String buildingMaterial;
+    private String standard;
+    private int squareMetres;
+    private long homeAmount;
+    private long contentsAmount;
+    
+    /**
+     * Creates a new home insurance with the given values.
+     * @param employee
+     * @param premium
+     * @param amount
+     * @param terms
+     * @param streetAddress
+     * @param zipCode
+     * @param homeType
+     * @param buildingMaterial
+     * @param standard
+     * @param squareMetres
+     * @param homeAmount
+     * @param contentsAmount
+     * @throws NullPointerException if any of the arguments are null
+     */
+    public HomeInsurance(Employee employee, long premium, long amount,
+            String terms, String streetAddress, String zipCode,
+            String homeType, String buildingMaterial, String standard,
+            int squareMetres, long homeAmount, long contentsAmount) {
+        super(employee, premium, amount, terms);
+        setStreetAddress(streetAddress);
+        setZipCode(zipCode);
+        setHomeType(homeType);
+        setBuildingMaterial(buildingMaterial);
+        setStandard(standard);
+        this.squareMetres = squareMetres;
+        this.homeAmount = homeAmount;
+        this.contentsAmount = contentsAmount;
+    }
+    
+    /**
+     * Returns the street address of the insured home.
+     * 
+     * @return the street address of the insured home
+     */
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    /**
+     * Sets the street address of the insured home to the given string. This
+     * method is private, because if the address is changed, it is not the same
+     * house, and a new insurance should be created.
+     * 
+     * @param streetAddress the new street address
+     * @throws NullPointerException if argument is null
+     */
+    private void setStreetAddress(String streetAddress) {
+        if (streetAddress == null) {
+            throw new NullPointerException("Street address cannot be null.");
+        }
+        this.streetAddress = streetAddress;
+    }
+
+    /**
+     * Returns the zip code of the insured home to the given value.
+     * 
+     * @return the zip code of the insured home
+     */
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * Sets the zip code of the insured home to the given string. This method
+     * is private, because if the zip code is changed, it is not the same
+     * house, and a new unsurance should be created.
+     * 
+     * @param zipCode the new zip code
+     * @throws NullPointerException if argument is null
+     */
+    public void setZipCode(String zipCode) {
+        if (zipCode == null) {
+            throw new NullPointerException("Zip code cannot be null.");
+        }
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * Returns the home type of the insured home.
+     * 
+     * @return the home type of the insured home
+     */
+    public String getHomeType() {
+        return homeType;
+    }
+
+    /**
+     * Sets the home type of the insured home to the given string. This method
+     * is private because if the home type is changed, it is no longer the same
+     * 
+     * @param homeType the new home type
+     * @throws NullPointerException if argument is null
+     */
+    private void setHomeType(String homeType) {
+        if (homeType == null) {
+            throw new NullPointerException("Home type cannot be null.");
+        }
+        this.homeType = homeType;
+    }
+
+    /**
+     * Returns the building material used to make the insured home.
+     * 
+     * @return the building material used to make the insured home
+     */
+    public String getBuildingMaterial() {
+        return buildingMaterial;
+    }
+
+    /**
+     * Sets the building material of the insured home to be the given value.
+     * This method is private because if the building material is changed, it
+     * is no longer the same house, and a new insurance should be created.
+     * 
+     * @param buildingMaterial the new building material
+     * @throws NullPointerException if argument is null
+     */
+    private void setBuildingMaterial(String buildingMaterial) {
+        if (buildingMaterial == null) {
+            throw new NullPointerException("Building material cannot be null");
+        }
+        this.buildingMaterial = buildingMaterial;
+    }
+
+    /**
+     * Returns the standard for the insured home.
+     * 
+     * @return the standard for the insured home
+     */
+    public String getStandard() {
+        return standard;
+    }
+
+    /**
+     * Sets the standard for the insured home to the given string.
+     * 
+     * @param standard the new standard
+     * @throws NullPointerException if argument is null
+     */
+    public void setStandard(String standard) {
+        if (standard == null) {
+            throw new NullPointerException("Standard cannot be null.");
+        }
+        this.standard = standard;
+    }
+
+    /**
+     * Returns the home amount for this home insurance.
+     * 
+     * @return the home amount for this home insurance
+     */
+    public long getHomeAmount() {
+        return homeAmount;
+    }
+
+    /**
+     * Sets the home amount for this home insurance to the given value.
+     * 
+     * @param homeAmount the new home amount
+     */
+    public void setHomeAmount(long homeAmount) {
+        this.homeAmount = homeAmount;
+    }
+
+    /**
+     * Returns the contents amount for this home insurance.
+     * 
+     * @return the contents amount for this home insurance
+     */
+    public long getContentsAmount() {
+        return contentsAmount;
+    }
+
+    /**
+     * Sets the contents amount for this home insurance to the given value.
+     * 
+     * @param contentsAmount the new home amount
+     */
+    public void setContentsAmount(long contentsAmount) {
+        this.contentsAmount = contentsAmount;
+    }
+
+    /**
+     * Returns the construction year for the insured home.
+     * 
+     * @return the construction year for the insured home
+     */
+    public int getConstructionYear() {
+        return constructionYear;
+    }
+
+    /**
+     * Returns the area of the insured home in square meters.
+     * 
+     * @return the area of the insured home in square meters
+     */
+    public int getSquareMetres() {
+        return squareMetres;
+    }
+}
