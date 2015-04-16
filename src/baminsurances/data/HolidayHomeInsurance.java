2eaed771d2dac75b91e7837e177b2f978341b970
Martin Jackson <martin.mellum.jackson@gmail.com>
2015-04-16 09:08:11 +0200

    Ground work finished for HolidayHomeInsurance.

---
+++ src/baminsurances/data/HolidayHomeInsurance.java
@@ -0,0 +1,58 @@
+package baminsurances.data;
+
+/**
+ * A class representing a holiday home insurance in the company's data bank.
+ * 
+ * @author martin
+ */
+public class HolidayHomeInsurance extends HomeInsurance {
+    private boolean rentedOut;
+
+    /**
+     * Creates a new holiday home insurance with the given values.
+     * 
+     * @param employee
+     * @param premium
+     * @param amount
+     * @param terms
+     * @param streetAddress
+     * @param zipCode
+     * @param homeType
+     * @param buildingMaterial
+     * @param standard
+     * @param squareMetres
+     * @param homeAmount
+     * @param contentsAmount
+     * @param rentedOut
+     * @throws NullPointerException if any of the arguments are null
+     */
+    public HolidayHomeInsurance(Employee employee, long premium, long amount,
+            String terms, String streetAddress, String zipCode,
+            String homeType, String buildingMaterial, String standard,
+            int squareMetres, long homeAmount, long contentsAmount,
+            boolean rentedOut) {
+        super(employee, premium, amount, terms, streetAddress, zipCode,
+                homeType, buildingMaterial, standard, squareMetres, homeAmount,
+                contentsAmount);
+        this.rentedOut = rentedOut;
+    }
+
+    /**
+     * Returns <code>true</code> if the insured holiday home is being rented
+     * out.
+     * 
+     * @return <code>true</code> if the insured holiday home is being rented
+     * out.
+     */
+    public boolean isRentedOut() {
+        return rentedOut;
+    }
+
+    /**
+     * 
+     * @param rentedOut
+     */
+    public void setRentedOut(boolean rentedOut) {
+        this.rentedOut = rentedOut;
+    }
+}
