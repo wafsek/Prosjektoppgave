dea438b53feb1a3ffafd7a2f88f6a956666d03dd
Martin Jackson <martin.mellum.jackson@gmail.com>
2015-05-02 16:39:36 +0200

    Changed 'postalCode' to 'zipCode' in Employee.java.

--- src/baminsurances/data/Employee.java
+++ src/baminsurances/data/Employee.java
@@ -8,8 +8,8 @@ package baminsurances.data;
  */
 public class Employee extends Person {
     public Employee(String birthNo, String firstName, String lastName,
-            String telephoneNo, String postalCode, String streetAddress) {
-        super(birthNo, firstName, lastName, telephoneNo, postalCode,
+            String telephoneNo, String zipCode, String streetAddress) {
+        super(birthNo, firstName, lastName, telephoneNo, zipCode,
                 streetAddress);
     }
 
